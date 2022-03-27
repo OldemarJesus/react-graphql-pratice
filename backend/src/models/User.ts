0cc4fab512e2b887464af147636d2d27ca9faa20
OldemarJesus <oldemego@gmail.com>
2022-03-27 03:34:08 +0100

    create and search added

---
+++ backend/src/models/User.ts
@@ -0,0 +1,11 @@
+import { __Type } from "graphql";
+import { Field, ID, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class User {
+    @Field(__Type => ID)
+    id: string
+
+    @Field()
+    name: string
+}
